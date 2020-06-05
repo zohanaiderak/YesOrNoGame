709d823286d5a513fb000efad9d3e93eab3d516d
Zohan Momin <zohanaiderak@gmail.com>
2020-06-05 11:44:22 -0400

    with sounds

--- src/Pages/Page.js
+++ src/Pages/Page.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import axios from 'axios';
-import './Page.scss'
+import './Page.scss';
+import Sound from 'react-sound';
+import Sound1 from '../assets/audio/kids-cheering-sound-effect.mp3';
+import Sound2 from '../assets/audio/nononono-cat-mp3cut.mp3'
 
 class Form extends React.Component{
     state={
         output:"",
         answer:"",
-        className: "hidden"
+        className: "hidden",
+        sound: ""
     }
 
     handleSubmit = (e) =>{
@@ -14,12 +18,26 @@ class Form extends React.Component{
 
         axios.get('https://yesno.wtf/api/')
             .then(res =>{
-                console.log(res.data)
                 this.setState({
                     output : res.data.image,
                     answer : res.data.answer,
                     className: "visible"
                 })
+                if(res.data.answer=="yes"){
+                    this.setState({
+                        sound : Sound1
+
+                    })
+                }
+                else if(res.data.answer=="no"){
+                    this.setState({
+                        sound : Sound2
+                    })
+                }else{
+                    this.setState({
+                        sound : "https://www.youtube.com/watch?v=xUVKrQ75jVY"
+                    })
+                }
             })
             .catch(error=>{
                 window.alert(error)
@@ -35,14 +53,18 @@ class Form extends React.Component{
     
     
     render(){
+        console.log()
         return(
             <form onSubmit={this.handleSubmit}>
                 <input type="text"></input>
+                    <h1>{this.state.answer}</h1>
                     <img className={this.state.className} height="100px" width="100px" src={this.state.output}></img>
                 <button type="submit">Get Answer!</button>
+                <Sound url={this.state.sound} playStatus={Sound.status.PLAYING} />
+                {/* <audio><source src='../assets/audio/kids-cheering-sound-effect.mp3'></source></audio> */}
                 <button onClick={this.reset} type="reset">Reset</button>
+                
             </form>
-
         )
     }
 }
